d137eae0f02d7bb7aba17551b6dda83ed8d71873
David Colatti <davidcolatti@gmail.com>
2021-05-25 16:08:25 -0400

    feat: multithreading with clusters

---
+++ index.js
@@ -0,0 +1,30 @@
+const cluster = require("cluster");
+const http = require("http");
+const forks = require("os").cpus().length;
+
+const scraper = require("./scraper");
+
+const main = async (id, count) => {
+  if (cluster.isMaster) {
+    console.log(`Master: [${process.pid}] is running`);
+
+    for (let i = 0; i < forks; i++) {
+      cluster.fork();
+    }
+
+    cluster.on("exit", (worker, code, signal) => {
+      console.log(`Worker [${worker.process.pid}] died`);
+    });
+  } else {
+    const workerId = cluster.worker.id;
+
+    console.log(`Worker ${workerId}: [${process.pid}] is running`);
+
+    const threadCount = count / forks;
+    const startId = (id += threadCount * (workerId - 1));
+
+    await scraper(startId, threadCount, workerId);
+  }
+};
+
+main(37858974, 10000);
